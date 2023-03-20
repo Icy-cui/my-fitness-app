c719b83284a13b8cb25a49c8d6c13276efa67047
Icy-cui <icercyf@gmail.com>
2023-03-20 16:27:48 +1100

    init: Home page and add new components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Box } from "@mui/material";
+import Exercises from "../components/Exercises";
+import HeroBanner from "../components/HeroBanner";
+import SearchExercises from "../components/SearchExercises";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  );
 }
