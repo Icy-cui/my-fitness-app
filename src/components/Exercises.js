738d24ccbd32e8ea4b1047b0a49376ebd0a4fabe
Icy-cui <icercyf@gmail.com>
2023-03-23 14:01:33 +1100

    Update: fetch exercise data by key words

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,8 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Pagination, Box, Stack, Typography } from "@mui/material";
 import { exerciseOptions, fetchData } from "../utils/fetchData";
+import ExerciseCard from "./ExerciseCard";
 
 export default function Exercises({ exercises, setExercises, bodyPart }) {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [exercisesPerPage] = useState(6);
+
+  // Pagination 分页
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
+
+  const paginate = (event, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1800, behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    console.log(exercises);
+    // 点击 horizontal bar 获取对应的数据, 数据改变，下面的 card 就改变
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+      if (bodyPart === "all") {
+        // all data
+        exercisesData = await fetchData("https://exercisedb.p.rapidapi.com/exercises", exerciseOptions);
+      } else {
+        // other data
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,exerciseOptions);
+      }
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
+
   return (
     <Box id="exercises" sx={{ mt: { lg: "109px" } }} mt="50px" p="20px">
       <Typography
@@ -19,12 +51,23 @@ export default function Exercises({ exercises, setExercises, bodyPart }) {
         flexWrap="wrap"
         justifyContent="center"
       >
-        <Stack sx={{ mt: { lg: "114px", xs: "70px" } }} alignItems="center">
-          {/* test output of api */}
-          {exercises.map((exercise, index) => (
-            <p>{exercise.name}</p>
-          ))}
-        </Stack>
+        {currentExercises.map((exercise, index) => (
+          // <p>{exercise.name}</p>
+          <ExerciseCard key={index} exercise={exercise} />
+        ))}
+      </Stack>
+      <Stack sx={{ mt: { lg: "114px", xs: "70px" } }} alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / 9)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
       </Stack>
     </Box>
   );
