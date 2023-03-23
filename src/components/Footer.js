14d521fe8e5653371f39f8a1e15f1f15e8e07eaf
Icy-cui <icercyf@gmail.com>
2023-03-23 15:53:00 +1100

    Update: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Logo from "../assets/images/Logo-1.png";
 
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+  <Box mt="80px" bgcolor="#FFF3F4">
+    <Stack
+      gap="40px"
+      sx={{ alignItems: "center" }}
+      flexWrap="wrap"
+      px="40px"
+      pt="24px"
+    >
+      <img src={Logo} alt="logo" style={{ width: "200px", height: "41px" }} />
+    </Stack>
+    <Typography sx={{ color: "gray" }} mt="30px" textAlign="center" pb="20px">
+      &copy; Copyright Red Black Tree .Co
+    </Typography>
+  </Box>;
 }
