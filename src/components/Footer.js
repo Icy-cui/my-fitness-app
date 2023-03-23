6e01b11d9d0ed4145f2ef63da5d8c660292848c5
Icy-cui <icercyf@gmail.com>
2023-03-23 16:35:52 +1100

    fix: footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,14 @@
-import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
-import Logo from "../assets/images/Logo-1.png";
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Logo from '../assets/images/Logo-1.png';
 
-export default function Footer() {
+const Footer = () => (
   <Box mt="80px" bgcolor="#FFF3F4">
-    <Stack
-      gap="40px"
-      sx={{ alignItems: "center" }}
-      flexWrap="wrap"
-      px="40px"
-      pt="24px"
-    >
-      <img src={Logo} alt="logo" style={{ width: "200px", height: "41px" }} />
+    <Stack gap="40px" sx={{ alignItems: 'center' }} flexWrap="wrap" px="40px" pt="24px">
+      <img src={Logo} alt="logo" style={{ width: '200px', height: '41px' }} />
     </Stack>
-    <Typography sx={{ color: "gray" }} mt="30px" textAlign="center" pb="20px">
-      &copy; Copyright Red Black Tree .Co
-    </Typography>
-  </Box>;
-}
+    <Typography sx={{ color: "gray" }} mt="20px" textAlign="center" pb="20px">&copy; CopyRight RBTree</Typography>
+  </Box>
+);
+
+export default Footer;
